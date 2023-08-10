dd3aa67c22edf4b1ddde5282e91a8079188a2ec8
Priyanka <priyankabehera312004@gmail.com>
2023-08-10 23:40:46 +0530

    kittu

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ console.log("hello world")
 let a = 57;
 //a contains 57
 console.log(a)
+let b = 77;
+//b contains 77
+console.log(b)
+let c = "priyanka";
+{
+    let c = "suraj";
+    console.log(c)
+}
+console.log(c)
+let d="Kittu";
+{
+    let d = "radhe";
+    console.log(d)
+}
+console.log(d)
